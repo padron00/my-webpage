95257d718fa08adac9c98810801d857e77f84890
Francisco Emanuel Portilla Salazar <pacofep16@gmail.com>
2022-01-17 20:28:19 -0600

    Student table added

---
+++ index.js
@@ -0,0 +1,29 @@
+var students = [
+  {
+    lname: "Portilla",
+    name: "Francisco Emanuel",
+    age: 26,
+    birthday: {
+      year: 1995,
+      month: "November",
+      day: 16
+    },
+    Career: "IDYGS"
+  }
+]
+
+var table = document.getElementsByTagName("table")[0];
+for (var i = 0; i < students.length; i++) {
+  var templ = document.getElementById("tbodytempl").content.cloneNode(true);
+  var tdnumber = templ.querySelector("span[name='tbodynumber']");
+  var tdname = templ.querySelector("span[name='tbodyname']");
+  var tdage = templ.querySelector("span[name='tbodyage']");
+  var tdcareer = templ.querySelector("span[name='tbodycareer']");
+
+  tdnumber.innerText = i + 1;
+  tdname.innerText = students[i].lname + " " + students[i].name;
+  tdage.innerText = students[i].age;
+  tdcareer.innerText = students[i].Career;
+
+  table.append(templ);
+}

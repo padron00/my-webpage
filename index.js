05eae87a53bfd04ff7cb0cbf98093f6d853d7055
CelsoReyes2629 <celso.reyesu@uttn.mx>
2022-01-17 20:43:53 -0600

    New element added to students array.

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ var students = [
       day: 16
     },
     Career: "IDYGS"
+  },
+  {
+    lname: "Reyes",
+    name: "Celso",
+    age: 21,
+    birthday: {
+      year: 2000,
+      month: "April",
+      day: 26
+    },
+    Career: "IDYGS"
   }
 ]
 
